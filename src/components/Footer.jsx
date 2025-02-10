0ce958ac8c2c18ed21e7fd3b472cf9b46be30d70
MAHESH KUMAR NAYAK <151497328+Maheshna369@users.noreply.github.com>
2025-02-10 13:30:50 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import logo from '../assets/reactIcon.png';
 const Footer=()=>{
     return(
         <div className="footer-container">
-            <h4>A Fully React App <img src={logo} alt="" /> </h4>
-            <h4>&copy; 2024 Maphy, Inc </h4>
+            <h4>A Fully React App</h4>
+            <h4>&copy; 2024-2025 Maphy, Inc </h4>
         </div>
     )
 }
